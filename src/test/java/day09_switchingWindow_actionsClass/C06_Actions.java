1e05c5a38a92ee95b8aa2b0abc8a741445ae0412
Merve Nur Tasan <127309624+mervenurtasann@users.noreply.github.com>
2023-05-11 22:50:44 +0300

    add

---
+++ src/test/java/day09_switchingWindow_actionsClass/C06_Actions.java
@@ -0,0 +1,33 @@
+package day09_switchingWindow_actionsClass;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.devtools.v85.network.model.DataReceived;
+import org.openqa.selenium.interactions.Actions;
+import utilities.TestBase;
+
+public class C06_Actions extends TestBase {
+    @Test
+    public void test01(){
+       // 1- https://www.amazon.com/ adresine gidin
+        driver.get("https://www.amazon.com/ ");
+        //2- Sag ust bolumde bulunan “Account & Lists” menusunun acilmasi icin mouse’u bu menunun ustune getirin
+
+        Actions actions = new Actions(driver);
+        WebElement accountsListMenuElementi = driver.findElement(By.xpath("//*[@id='nav-link-accountList']"));
+        actions.moveToElement(accountsListMenuElementi).perform();
+
+        //3- “Create a list” butonuna basin
+        WebElement createListElementi = driver.findElement(By.xpath("//*[text()='Create a List']"));
+        createListElementi.click();
+
+        //4- Acilan sayfada “Your Lists” yazisi oldugunu test edin
+        WebElement yourListsElementi = driver.findElement(By.xpath("//div[@role='heading']"));
+        String expectedYazi = "Your Lists";
+        String actualYazi = yourListsElementi.getText();
+
+        Assert.assertEquals(expectedYazi,actualYazi);
+    }
+}

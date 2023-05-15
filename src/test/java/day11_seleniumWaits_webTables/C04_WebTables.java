be6d1a41b51aafdc52ba6e71cfe22615cfb3ef4a
Merve Nur Tasan <127309624+mervenurtasann@users.noreply.github.com>
2023-05-16 02:36:34 +0300

    add

--- src/test/java/day11_seleniumWaits_webTables/C04_WebTables.java
+++ src/test/java/day11_seleniumWaits_webTables/C04_WebTables.java
@@ -2,4 +2,26 @@ package day11_seleniumWaits_webTables;
 
 public class C04_WebTables {
 
+    /*
+        HTML kodlari ile olusturulmus
+        excel'e benzeyen
+        satirlar ve satirlardaki datalardan olusan yapidir
+
+        Front-end developer'lar
+        web-tablolarini farkli kod bloklari ile olusturulabilirler
+        ama HTML'in temelinde web-table olusturmak icin kullanilan yapi su sekildedir
+        table
+            thead
+                tr
+                    td
+            tbody
+                tr
+                    td
+         web-table'larda locate islemi icin
+         absolute-xpath'e benzer bir sekilde
+
+         //table/tbody/tr/td
+         siralamasi ile locate yapariz
+         tr ve td icin [index] kullanilabilir
+     */
 }
